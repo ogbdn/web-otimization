939f9a521952788a2ab53536b0eb1a7abefff438
Othon G. Nin <othon.nin@gmail.com>
2018-04-25 20:37:43 -0300

    Trabalho final otimizado

--- views/js/main.js
+++ views/js/main.js
@@ -449,10 +449,12 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+    var seletor = document.getElementsByClassName("randomPizzaContainer");
+    var tamanho = seletor.length;
+    var dx = determineDx(seletor[0], size);
+    var newwidth = seletor[0].offsetWidth + dx + 'px';
+    for (var i = 0; i < tamanho; i++) {
+      seletor[i].style.width = newwidth;
     }
   }
 
@@ -501,7 +503,7 @@ function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
 
-  var items = document.querySelectorAll('.mover');
+  var items = document.getElementsByClassName('mover');
   for (var i = 0; i < items.length; i++) {
     // document.body.scrollTop is no longer supported in Chrome.
     var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
@@ -526,7 +528,7 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
-  for (var i = 0; i < 200; i++) {
+  for (var i = 0; i < 50; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
